b661b6b0fc74a6f5ad9c65afadbd22f096a0a27a
Andrew Hamilton <andrew.hamilton@gmail.com>
2017-05-09 16:33:19 -0400

    fixed .gitignore error, updated deploy and production files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server '127.0.0.1', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
