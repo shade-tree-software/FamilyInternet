e86e24af7f9e4769fda1cab3685634ed5dca0b79
Andrew Hamilton <andrew.hamilton@gmail.com>
2017-05-10 14:10:56 -0400

    cleaned up pages and added ajax for countdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,19 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @users}
+    end
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    respond_to do |format|
+      format.html
+      format.json {render json: @user}
+    end
   end
 
   # GET /users/new
@@ -28,7 +36,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to action: "index", notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -42,7 +50,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to action: "index", notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   def self.update_countdown
 	  User.where(active: true).each do |user|
 	    if user.countdown > 0
-	      user.countdown = user.countdown - 60 
+	      new_countdown = user.countdown - 60
+	      new_countdown = 0 if new_countdown < 0
+	      user.countdown = new_countdown  
 	      user.save
 	    end
 	    if user.countdown <= 0
+	      user.countdown = 0 if user.countdown < 0
 	      user.active = false
 	      user.save
 	    end
