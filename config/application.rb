ae6768bb727b4f828e77b8caa5e69a6532ff2e7c
Andrew Hamilton <andrew.hamilton@gmail.com>
2017-05-11 10:42:21 -0400

    bootstrap working now, also added slide toggles

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module FamilyInternet
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'css')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
